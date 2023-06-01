de741b425747b96bbdd6a42b45b461de7475bba6
prashanth9100 <pvprashanth030@gmail.com>
2023-05-31 23:21:13 -0500

    commit #1

---
+++ Student1/src/Main.java
@@ -0,0 +1,24 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+
+   private String name, newName;
+    public String getName() {
+        return name;
+    }
+    public void setName(String newName) {
+        this.name = newName;
+    }
+
+    public static void main(String[] args) {
+
+
+
+
+        Main myObj = new Main();
+        myObj.setName("John");
+        System.out.println(myObj.getName());
+    }
+}
+
+
