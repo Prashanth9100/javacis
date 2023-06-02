f1056de10a37408ecdd70cca73511faf990851a7
prashanth9100 <pvprashanth030@gmail.com>
2023-06-01 19:40:27 -0500

    commit #2

---
+++ Student1/src/Student.java
@@ -0,0 +1,19 @@
+public class Student {
+    private String name, newName;
+    public String getName() {
+        return name;
+    }
+    public void setName(String newName) {
+        this.name = newName;
+    }
+
+    public static void main(String[] args) {
+
+
+
+
+        Student myObj = new Student();
+        myObj.setName("PrashanthP");
+        System.out.println(myObj.getName());
+    }
+}
