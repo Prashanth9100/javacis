119616b2e0ce2a5c634dcee123d881c3bd6b8076
prashanth9100 <pvprashanth030@gmail.com>
2023-06-07 15:51:32 -0500

    Commit #7 07-05-2023

--- Student1/src/array/First.java
+++ Student1/src/array/First.java
@@ -3,31 +3,26 @@ package array;
 
 public class First {
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         int large = 0;
         int small = 0;
         int arr[] = new int[]{1, 25, 5, 89, 55, 6};
 
-        for(int i=0; i<arr.length;i++)
-        {
-            if(arr[i]>large)
-            {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > large) {
                 large = arr[i];
             }
-        }
-        small = large;
-        for(int i=0; i<arr.length;i++)
-        {
-            if(arr[i]<small)
-            {
+
+
+            if (arr[i] < large) {
                 small = arr[i];
             }
         }
-        System.out.println("Smallest number:"+small);
-        System.out.println("largest number:"+large);
+
+            System.out.println("Smallest number:" + small);
+            System.out.println("largest number:" + large);
 
 
-
+        }
     }
-}
+
